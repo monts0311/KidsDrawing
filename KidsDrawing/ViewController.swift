160f81e4edebe8ded324786c61f963343f1a3cf9
monts0311 <monts0311@gmail.com>
2017-08-17 14:31:55 +0900

    Initial Commit

---
+++ KidsDrawing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KidsDrawing
+//
+//  Created by HONGYONG SOO on 2017. 8. 17..
+//  Copyright © 2017년 com.monts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
