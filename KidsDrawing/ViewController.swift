6a48065eccc791538f70dfb60198588cb9907e86
monts0311 <monts0311@gmail.com>
2017-08-18 17:45:22 +0900

    - Alamofire 추가 및 통신 추가

--- KidsDrawing/ViewController.swift
+++ KidsDrawing/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    // MARK: - Const
+    let SERVER_URL:String = "http://kidsking.co/hong/"
+    let SERVER_LIST_FILE:String = "list_test.json"
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        requestImageList()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+extension ViewController {
+    func requestImageList() {
+        Alamofire.request(SERVER_URL + SERVER_LIST_FILE).responseJSON { response in
+            if let json = response.result.value {
+                print("JSON: \(json)")
+            }
+        }
+    }
+}
