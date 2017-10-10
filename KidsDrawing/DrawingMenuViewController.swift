c7072e712e3deab73143cb89fe40f03c64ecc8b6
monts0311 <monts0311@gmail.com>
2017-10-10 17:47:18 +0900

    - Collection View 적용 중...

--- KidsDrawing/DrawingMenuViewController.swift
+++ KidsDrawing/DrawingMenuViewController.swift
@@ -8,10 +8,53 @@
 
 import UIKit
 
-class DrawingMenuViewController : UIViewController {
+class DrawingMenuViewController : UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    // MARK: - IBOutlet
+    @IBOutlet weak var mCollectionView: UICollectionView!
     
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mCollectionView.delegate = self
+        mCollectionView.dataSource = self
+    }
+
     // MARK: - IBAction
     @IBAction func onBack(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: - DataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "image",
+                                                      for: indexPath)
+        cell.backgroundColor = UIColor.black
+        // Configure the cell
+        return cell
+    }
+    
+    // MARK: - Delegate
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = 150
+        let cellHeight = 126
+        
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(20,20,20,20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
     }
 }
